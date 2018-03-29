50b14a7286e9bea2f9a7dbdd212f1f51f7063d88
Peter Schoener <peter.c.schoener@gmail.com>
2018-03-29 16:55:48 -0700

    optimized preprocessing script
    script now keeps the most recent DOM in memory; speeds it up by like 50x on my machine

--- preprocess.py
+++ preprocess.py
@@ -6,8 +6,10 @@ import os
 directory_path = "./directory/"
 input_path = "./input/"
 output_path = "./preprocessed/"
+ns = {"xsi" : "http://www.w3.org/2001/XMLSchema-instance"} # namespace for all of the XML files
 
 blacklist = {""} # blacklist failed files; don't retry opening them
+last_file = ""
 
 # go through all the directory files
 for filename in os.listdir(directory_path) :
@@ -18,18 +20,24 @@ for filename in os.listdir(directory_path) :
 	for line in directory_file.read().split("\n") :	
 		# ? filename_identifier ? house yyyy_mm_dd ? party title
 		fields = line.split()
+		
+		if len(fields) <= 4 or len(fields[1].split("_")) <= 1 :
+			continue
+		
 		id = fields[1].split("_")
 		year_str = fields[4].split("_")[0]
 		
 		if id[0] not in blacklist :
 			try :
-				# parse tree of src doc and open target year file
-				source_tree = etree.parse(input_path + id[0] + ".xml")
-				root = source_tree.getroot()
+				if id[0] != last_file : # so that the same file isn't always closed and reopened
+					# parse tree of src doc and open target year file
+					source_tree = etree.parse(input_path + id[0] + ".xml")
+					root = source_tree.getroot()
+					last_file = id[0]
+				
 				aggregate_file = open(output_path + year_str + ".txt", "a")
 				
 				# find element by id using XPath
-				ns = {"xsi" : "http://www.w3.org/2001/XMLSchema-instance"}
 				element = root.find(".//*[@id='" + id[0] + "-" + id[1] + "']", ns)
 				
 				# if the element exists, add its contents to the aggregate (with a delimiter in case one is not already present)
