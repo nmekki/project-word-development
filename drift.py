1fb3699ca194e77ea1d66d20dd7358f4fd1930db
Peter Schoener <peter.c.schoener@gmail.com>
2018-03-30 15:18:02 -0700

    edited drift script for new preprocessed format
    now also saves and frees memory intermittently
    also fixed (added) tokenization

--- drift.py
+++ drift.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import sys
 import gensim
 
 # constants
@@ -11,42 +12,60 @@ input_dir = "./input"
 sentence_sets = {}
 models = {}
 
-# read input files
-print("reading...")
-for filename in os.listdir(input_dir) :
-	input = open(filename).read()
-	
-	# split year data off
-	year = int(input[:4])
-	input = input[5:]
-	
-	# split by sentences
-	input = input.replace("!", ".")
-	input = input.replace("?", ".")
-	sentences = input.split(".")
-	
-	# add these sentences to every set in the time window
-	for y in range(year, year + window_len) :
-		if sentence_sets[y] == None : # `if ... not in ...` ?
-			sentence_sets[y] = []
-		sentence_sets[y] += sentences
+# parse args
+if len(sys.argv) <= 3 :
+	print("Usage: drift.py [start_year] [end_year] [window_len]")
+start_year = int(sys.argv[0])
+end_year = int(sys.argv[1])
+window_len = int(sys.argv[2])
 
-print("making models...")
-for year in sentence_sets :
-	print(year + "\r")
-	# turn sentence sets into embedding models
-	model = Word2Vec(sentence_sets[year], size=100, window=5, min_count=5, workers=4)
+# make models
+print("making models", end = "\r")
+year_range = end_year + 1 - start_year
+i = 1
+for year in range(start_year, end_year + 1) :
+	try :
+		input = open(str(year) + ".txt")
+		
+		# split by sentences
+		text = input.read()
+		text = text.replace("!", ".")
+		text = text.replace("?", ".")
+		sentences = text.split(".")
+		sentences = [sent.split() for sent in sentences] # tokenize
+		
+		# add these sentences to every set in the time window
+		for y in range(year, year + window_len) :
+			if y not in sentence_sets :
+				sentence_sets[y] = []
+			sentence_sets[y] += sentences
 	
-	# save the models just in case
-	model.save(str(year) + "-" + str(window_len) + ".word2vec")
+	except :
+		print("Could not find data for " + str(year) + " (" + str(year) + ".txt); skipping")
+
+	# make embedding model regardless of whether data for this year was found (use windows)
+	# however, there must be something in the set or else this won't work; fail if empty
+	if len(sentence_sets[year]) == 0 :
+		print("No data in window for " + str(year), file = sys.stderr)
+		sys.exit(1)
+	else :
+		model = Word2Vec(sentence_sets[year], size = 100, window = 5, min_count = 5, workers = 4)
+		model.save(str(year) + "+" + str(window_len) + ".word2vec")
+		
+		# clear sentence set and model from memory
+		del(model)
+	del(sentence_sets[year])
 	
-	# associate compact model with year
-	models[year] = model.wv
-	
+	print("making models (%d/%d)" % (i, year_range), end = "\r")
+	i += 1
+print()
+
 # consider only words that are in all models
-print("finding overlap...")
+print("finding overlap...", end = "\r")
 base = models[[m for m in models][0]] # there is probably a better way to do this but this gets the first model
 wordlist = []
+i = 1
+p = 0
 for word in base :
 	add = True
 	for model in models :
@@ -55,6 +74,12 @@ for word in base :
 			break
 	if add :
 		wordlist += [word]
+	
+	i += 1
+	if (100 * i // len(base)) > p :
+		p = 100 * i // len(base)
+		print("finding overlap (%d%%)" % (p), end = "\r")
+print()
 
 # go through all candidate words
 print("building neighbor lists...")
