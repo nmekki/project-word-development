f67f38075e321aab64dd8378bc96b5af9f86c2c6
Peter Schoener <peter.c.schoener@gmail.com>
2018-03-31 11:02:11 -0700

    bugfixes
    type error fixes
    set operation cleanup
    neighbor printing cleanup
    model access fixes
    file write clarification
    format string cleanup

--- drift.py
+++ drift.py
@@ -28,61 +28,69 @@ if end_year < start_year :
 	sys.exit(2)
 
 # make models
-print("making models", end = "\r")
-year_range = end_year + 1 - start_year
-i = 1
-for year in range(start_year, end_year + 1) :
-	try :
-		input = open(input_dir + str(year) + ".txt")
+# print("making models", end = "\r")
+# year_range = end_year + 1 - start_year
+# i = 1
+# for year in range(start_year, end_year + 1) :
+	# try :
+		# input = open(input_dir + str(year) + ".txt")
 		
-		# split by sentences
-		text = input.read()
-		text = text.replace("!", ".")
-		text = text.replace("?", ".")
-		sentences = text.split(".")
-		sentences = [sent.split() for sent in sentences] # tokenize
+		# # split by sentences
+		# text = input.read()
+		# text = text.replace("!", ".")
+		# text = text.replace("?", ".")
+		# sentences = text.split(".")
+		# sentences = [sent.split() for sent in sentences] # tokenize
 		
-		# add these sentences to every set in the time window
-		for y in range(year, year + window_len) :
-			if y not in sentence_sets :
-				sentence_sets[y] = []
-			sentence_sets[y] += sentences
+		# # add these sentences to every set in the time window
+		# for y in range(year, year + window_len) :
+			# if y not in sentence_sets :
+				# sentence_sets[y] = []
+			# sentence_sets[y] += sentences
 	
-	except :
-		print("Could not find data for " + str(year) + " (" + str(year) + ".txt); skipping")
+	# except :
+		# print("Could not find data for " + str(year) + " (" + str(year) + ".txt); skipping")
 
-	# make embedding model regardless of whether data for this year was found (use windows)
-	# however, there must be something in the set or else this won't work; fail if empty
-	if len(sentence_sets[year]) == 0 :
-		print("No data in window for " + str(year), file = sys.stderr)
-		sys.exit(1)
-	else :
-		model = gensim.models.Word2Vec(sentence_sets[year], size = dimensionality, window = 5, min_count = 5, workers = 4)
-		model.save(output_dir + str(year) + "+" + str(window_len) + ".word2vec")
+	# # make embedding model regardless of whether data for this year was found (use windows)
+	# # however, there must be something in the set or else this won't work; fail if empty
+	# if len(sentence_sets[year]) == 0 :
+		# print("Fatal: No data in window for " + str(year), file = sys.stderr)
+		# sys.exit(1)
+	# else :
+		# model = gensim.models.Word2Vec(sentence_sets[year], size = dimensionality, window = 5, min_count = 5, workers = 4)
+		# model.save(output_dir + str(year) + "+" + str(window_len) + ".word2vec")
 		
-		# clear sentence set and model from memory
-		#del(model)
-	del(sentence_sets[year])
+		# # clear sentence set and model from memory
+		# #del(model)
+	# del(sentence_sets[year])
 	
-	print("making models (%d/%d)" % (i, year_range), end = "\r")
-	i += 1
-print()
-del(sentence_sets)
+	# print("making models (%d/%d)" % (i, year_range), end = "\r")
+	# i += 1
+# print()
+# del(sentence_sets)
+
+# intermittent load due to errors
+print("loading models", end = "\r")
+for year in range(start_year, end_year + 1) :
+	model = gensim.models.Word2Vec.load(output_dir + str(year) + "+" + str(window_len) + ".word2vec")
+	models[year] = model.wv
+	del(model)
+	print("loading models (" + str(year) + ")", end = "\r")
 
 # consider only words that are in all models
 print("finding overlap...", end = "\r")
-base = list(models.values())[0] # there is probably a better way to do this but this gets the first model
-wordset = []
+base = list(models.values())[0].vocab
+wordset = set()
 i = 1
 p = 0
 for word in base :
 	add = True
-	for model in models :
+	for model in models.values() :
 		if word not in model :
 			add = False
 			break
 	if add :
-		wordset |= {word}
+		wordset.add(word)
 	
 	i += 1
 	if (100 * i // len(base)) > p :
@@ -96,15 +104,23 @@ pickle.dump(wordset, output)
 output.close()
 
 # go through all candidate words
-print("building neighbor lists...")
+print("building neighbor lists...", end = "\r")
 word_scores = {}
+i = 1
+p = 0
 for word in wordset :
 	top_tens_union = set()
-	for model in models : # generate the set union over all models of the top ten lists
-		top_tens_union |= set(model.most_similar(positive=[word], topn=10))
+	for model in models.values() : # generate the set union over all models of the top ten lists
+		top_tens_union |= set(model.most_similar(positive = [word], topn = 10))
 	
 	# test metric: union cardinality
 	word_scores[word] = len(top_tens_union)
+	
+	i += 1
+	if (100 * i // len(wordset)) > p :
+		p = (100 * i // len(wordset))
+		print("building neighbor lists (%d%%)" % (p), end = "\r")
+print()
 
 # sort list
 print("sorting...")
