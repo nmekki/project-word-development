81907452302ea6a44354067d2425a985461919ce
Peter Schoener <peter.c.schoener@gmail.com>
2018-04-06 19:58:21 -0700

    added new metric
    also updated sentence splitting and tokenization to use NLTK methods

--- drift.py
+++ drift.py
@@ -4,6 +4,7 @@ import os
 import sys
 import gensim.models
 import pickle
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 # constants
 window_len = 1
@@ -35,12 +36,11 @@ for year in range(start_year, end_year + 1) :
 	try :
 		input = open(input_dir + str(year) + ".txt")
 		
-		# split by sentences
+		# normalize, split by sentences
 		text = input.read()
-		text = text.replace("!", ".")
-		text = text.replace("?", ".")
-		sentences = text.split(".")
-		sentences = [sent.split() for sent in sentences] # tokenize
+		text = text.lower()
+		sentences = sent_tokenize(text)
+		sentences = [word_tokenize(sent) for sent in sentences]
 		
 		# add these sentences to every set in the time window
 		for y in range(year, year + window_len) :
@@ -60,8 +60,7 @@ for year in range(start_year, end_year + 1) :
 		model = gensim.models.Word2Vec(sentence_sets[year], size = dimensionality, window = 5, min_count = 5, workers = 4)
 		model.save("%s%d+%sx%d.word2vec" % (output_dir, year, sys.argv[3], dimensionality))
 		
-		# clear sentence set and model from memory
-		#del(model)
+	# clear sentence set from memory
 	del(sentence_sets[year])
 	
 	print("Making models (%d/%d)" % (i, year_range), end = "\r")
@@ -70,12 +69,12 @@ print()
 del(sentence_sets)
 
 # # intermittent load due to errors
-# print("loading models", end = "\r")
+# print("Loading models...", end = "\r")
 # for year in range(start_year, end_year + 1) :
 	# model = gensim.models.Word2Vec.load("%s%d+%sx%d.word2vec" % (output_dir, year, sys.argv[3], dimensionality))
 	# models[year] = model.wv
 	# del(model)
-	# print("loading models (" + str(year) + ")", end = "\r")
+	# print("Loading models (" + str(year) + ")", end = "\r")
 
 # consider only words that are in all models
 print("Finding overlap...", end = "\r")
@@ -104,27 +103,50 @@ pickle.dump(wordset, output)
 output.close()
 
 # go through all candidate words
-print("Building neighbor lists...", end = "\r")
-word_scores = {}
-i = 1
-p = 0
-for word in wordset :
-	top_tens_union = set()
-	for model in models.values() : # generate the set union over all models of the top ten lists
-		top_tens_union |= set(model.most_similar(positive = [word], topn = 10))
+# print("Building neighbor lists...", end = "\r")
+# word_scores = {}
+# i = 1
+# p = 0
+# for word in wordset :
+	# top_tens_union = set()
+	# for model in models.values() : # generate the set union over all models of the top ten lists
+		# top_tens_union |= set(model.most_similar(positive = [word], topn = 10))
 	
-	# test metric: union cardinality
-	word_scores[word] = len(top_tens_union)
+	# # test metric: union cardinality
+	# word_scores[word] = len(top_tens_union)
+	
+	# i += 1
+	# if (100 * i // len(wordset)) > p :
+		# p = (100 * i // len(wordset))
+		# print("Building neighbor lists (%d%%)" % (p), end = "\r")
+# print()
+
+for word in wordset :
+    print(word + "        ", end = "\r")
+    union = set()
+    rows = dict()
+    for year in range(start_year, end_year + 1) :
+        similar = models[year].most_similar(positive = [word], topn = 10)
+        union |= set([e[0] for e in similar])
+        rows[year] = dict(similar)
+    for year in rows :
+        for w in union :
+            if w not in rows[year] :
+                if w in models[year] :
+                    rows[year][w] = models[year].similarity(word, w)
+                else :
+                    rows[year][w] = 0
+    rows = numpy.array([[row[val] for val in sorted(row)] for row in list(rows.values())])
+    dict_metric2[word] = numpy.sum([numpy.std(row) for row in numpy.rot90(rows)])
 	
 	i += 1
 	if (100 * i // len(wordset)) > p :
 		p = (100 * i // len(wordset))
 		print("Building neighbor lists (%d%%)" % (p), end = "\r")
-print()
 
 # sort list
 print("Sorting...", end = "\r")
-drifters = sorted(word_scores, key=word_scores.get)
+drifters = sorted(word_scores, key = word_scores.get)
 del(word_scores)
 print("Sorted    ")
 
