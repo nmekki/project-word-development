9f9869aced10adc57f014a1ea2d8db889c1b676a
Peter Schoener <peter.c.schoener@gmail.com>
2018-04-12 09:22:37 -0700

    minor tweaks
    removed dead code
    added detailed output
    fixed string handling

--- drift.py
+++ drift.py
@@ -29,58 +29,58 @@ if end_year < start_year :
 	print("Fatal: End year must be after start year", file = sys.stderr)
 	sys.exit(2)
 
-# # make models
-# print("Making models...", end = "\r")
-# year_range = end_year + 1 - start_year
-# i = 1
-# for year in range(start_year, end_year + 1) :
-	# try :
-		# input = open(input_dir + str(year) + ".txt")
-		
-		# # normalize, split by sentences
-		# text = input.read()
-		# text = text.lower()
-		# sentences = sent_tokenize(text)
-		# sentences = [word_tokenize(sent) for sent in sentences]
-		
-		# # add these sentences to every set in the time window
-		# for y in range(year, year + window_len) :
-			# if y not in sentence_sets :
-				# sentence_sets[y] = []
-			# sentence_sets[y] += sentences
-	
-	# except :
-		# print("Could not find data for " + str(year) + " (" + str(year) + ".txt); skipping")
-
-	# # make embedding model regardless of whether data for this year was found (use windows)
-	# # however, there must be something in the set or else this won't work; fail if empty
-	# if len(sentence_sets[year]) == 0 :
-		# print("Fatal: No data in window for " + str(year), file = sys.stderr)
-		# sys.exit(1)
-	# else :
-		# model = gensim.models.Word2Vec(sentence_sets[year], size = dimensionality, window = 5, min_count = 5, workers = 4)
-		# model.save("%s%d+%sx%d.word2vec" % (output_dir, year, sys.argv[3], dimensionality))
-		
-	# # clear sentence set from memory
-	# del(sentence_sets[year])
-	
-	# print("Making models (%d/%d)" % (i, year_range), end = "\r")
-	# i += 1
-# print()
-# del(sentence_sets)
-
-# intermittent load due to errors
-print("Loading models...", end = "\r")
+# make models
+print("Making models...", end = "\r")
+year_range = end_year + 1 - start_year
+i = 1
 for year in range(start_year, end_year + 1) :
 	try :
-		model = gensim.models.Word2Vec.load("%s%d+%sx%d.word2vec" % (output_dir, year, sys.argv[3], dimensionality))
-		models[year] = model.wv
-		del(model)
-		print("Loading models (" + str(year) + ")", end = "\r")
+		input = open(input_dir + str(year) + ".txt")
+		
+		# normalize, split by sentences
+		text = input.read()
+		text = text.lower()
+		sentences = sent_tokenize(text)
+		sentences = [word_tokenize(sent) for sent in sentences]
+		
+		# add these sentences to every set in the time window
+		for y in range(year, year + window_len) :
+			if y not in sentence_sets :
+				sentence_sets[y] = []
+			sentence_sets[y] += sentences
+	
 	except :
-		print("Fatal: No model found for %d (%s%d+%sx%d.word2vec)" % (year, output_dir, year, sys.argv[3], dimensionality), file = sys.stderr)
-		sys.exit(4)
-print("Loading models (done)")
+		print("Could not find data for %d (%d.txt); skipping" % (year, year))
+
+	# make embedding model regardless of whether data for this year was found (use windows)
+	# however, there must be something in the set or else this won't work; fail if empty
+	if len(sentence_sets[year]) == 0 :
+		print("Fatal: No data in window for %d" % (year), file = sys.stderr)
+		sys.exit(1)
+	else :
+		model = gensim.models.Word2Vec(sentence_sets[year], size = dimensionality, window = 5, min_count = 5, workers = 4)
+		model.save("%s%d+%sx%d.word2vec" % (output_dir, year, sys.argv[3], dimensionality))
+		
+	# clear sentence set from memory
+	del(sentence_sets[year])
+	
+	print("Making models (%d/%d)" % (i, year_range), end = "\r")
+	i += 1
+print()
+del(sentence_sets)
+
+# # intermittent load due to errors
+# print("Loading models...", end = "\r")
+# for year in range(start_year, end_year + 1) :
+	# try :
+		# model = gensim.models.Word2Vec.load("%s%d+%sx%d.word2vec" % (output_dir, year, sys.argv[3], dimensionality))
+		# models[year] = model.wv
+		# del(model)
+		# print("Loading models (%d - %d)" % (start_year, year), end = "\r")
+	# except :
+		# print("Fatal: No model found for %d (%s%d+%sx%d.word2vec)" % (year, output_dir, year, sys.argv[3], dimensionality), file = sys.stderr)
+		# sys.exit(4)
+# print()
 
 # consider only words that are in all models
 print("Finding overlap...", end = "\r")
@@ -108,25 +108,6 @@ output = open(output_dir + "overlap-%s-%s+%sx%d" % (sys.argv[1], sys.argv[2], sy
 pickle.dump(wordset, output)
 output.close()
 
-# go through all candidate words
-# print("Calculating drift...", end = "\r")
-# word_scores = dict()
-# i = 1
-# p = 0
-# for word in wordset :
-	# top_tens_union = set()
-	# for model in models.values() : # generate the set union over all models of the top ten lists
-		# top_tens_union |= set(model.most_similar(positive = [word], topn = 10))
-	
-	# # test metric: union cardinality
-	# word_scores[word] = len(top_tens_union)
-	
-	# i += 1
-	# if (100 * i // len(wordset)) > p :
-		# p = (100 * i // len(wordset))
-		# print("Calculating drift (%d%%)" % (p), end = "\r")
-# print()
-
 i = 1
 p = 0
 dict_metric2 = dict()
@@ -144,8 +125,20 @@ for word in wordset :
 					rows[year][w] = models[year].similarity(word, w)
 				else :
 					rows[year][w] = 0
-	rows = numpy.array([[row[val] for val in sorted(row)] for row in list(rows.values())])
-	dict_metric2[word] = numpy.sum([numpy.std(row) for row in numpy.rot90(rows)])
+	cols = numpy.array([[row[val] for val in sorted(row)] for row in list(rows.values())])
+	dict_metric2[word] = numpy.sum([numpy.std(row) for row in numpy.rot90(cols)])
+	
+	# write exhaustive data to csv
+	try :
+		with open("%s%s-%s-%s+%sx%d.csv" % (output_dir, word, sys.argv[1], sys.argv[2], sys.argv[3], dimensionality), "w") as output :
+			print(",%s" % (",".join(map(str, range(start_year, end_year + 1)))), file = output)
+			for word in union :
+				print(word, file = output, end = ",")
+				print(",".join(map(str, [rows[year][word] for year in range(start_year, end_year + 1)])), file = output)
+			print("", file = output)
+			output.close()
+	except :
+		print("Error: could not write file %s%s-%s-%s+%sx%d.csv; skipping" % (output_dir, word, sys.argv[1], sys.argv[2], sys.argv[3], dimensionality), file = sys.stderr)
 	
 	i += 1
 	if (100 * i // len(wordset)) > p :
@@ -153,16 +146,10 @@ for word in wordset :
 		print("Calculating drift (%d%%)" % (p), end = "\r")
 print()
 
-# # sort list
-# print("Sorting...", end = "\r")
-# drifters = sorted(word_scores, key = word_scores.get)
-# del(word_scores)
-# print("Sorted    ")
-
 # sort list
 print("Sorting...", end = "\r")
 drifters = sorted(dict_metric2, key = dict_metric2.get)
-del(dict_metric2)
+#del(dict_metric2)
 print("Sorted    ")
 
 # save sorted list
