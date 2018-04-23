bdd7d8b0ae4654f80f0b1306f348f89dc9520b1c
Peter Schoener <peter.c.schoener@gmail.com>
2018-04-23 12:34:51 +0200

    cleaned up argument parsing and fixed model deletion

--- drift.py
+++ drift.py
@@ -2,33 +2,35 @@
 
 import os
 import sys
+import argparse
 import gensim.models
 import pickle
 from nltk.tokenize import sent_tokenize, word_tokenize
 import numpy
 
-# constants
-window_len = 1
-input_dir = "./preprocessed/"
-output_dir = "./output/"
-dimensionality = 50
+# parse command line args
+parser = argparse.ArgumentParser(description = "Processes semantic drift over time.")
+parser.add_argument("--input", "-i", default = "./preprocessed/", help = "the directory containing the text files", metavar = "input_dir")
+parser.add_argument("--output", "-o", default = "./output/", help = "the directory into which to place the embedding and result files", metavar = "output_dir")
+parser.add_argument("--smoothing", "-s", type = int, default = 1, help = "the amount of smoothing, in years")
+parser.add_argument("--topn", "-t", type = int, default = 10, help = "the amount of smoothing, in years")
+parser.add_argument("--csv", "-c", type = bool, default = False, help = "output .csv files with detailed information on each word")
+parser.add_argument("--dimensionality", "-d", type = int, default = 50, help = "dimensionality to use for embeddings")
+parser.add_argument("start_year", type = int, help = "the year from which to start calculating drift")
+parser.add_argument("end_year", type = int, help = "the year until which to calculate drift")
+ns = parser.parse_args()
+start_year = ns.start_year
+end_year = ns.end_year
+window_len = ns.smoothing
+input_dir = ns.input
+output_dir = ns.output
+dimensionality = ns.dimensionality
+csv = ns.csv
+top_n = ns.topn
 
 # map each time window to a sentence list and an embedding model
 sentence_sets = {}
 models = {}
-
-# parse args
-if len(sys.argv) <= 4 :
-	print("Usage: %s [start_year] [end_year] [window_len] [top_n] (--nocsv)" % sys.argv[0], file = sys.stderr)
-	sys.exit(3)
-start_year = int(sys.argv[1])
-end_year = int(sys.argv[2])
-window_len = int(sys.argv[3])
-top_n = int(sys.argv[4])
-if "--nocsv" in sys.argv :
-	csv = False
-else :
-	csv = True
 
 if end_year < start_year :
 	print("Fatal: End year must be after start year", file = sys.stderr)
@@ -64,7 +66,8 @@ for year in range(start_year, end_year + 1) :
 		sys.exit(1)
 	else :
 		model = gensim.models.Word2Vec(sentence_sets[year], size = dimensionality, window = 5, min_count = 5, workers = 4)
-		model.save("%s%d+%sx%d.word2vec" % (output_dir, year, sys.argv[3], dimensionality))
+		model.save("%s%d+%dx%d.word2vec" % (output_dir, year, window_len, dimensionality))
+		models[year] = model.wv
 		
 	# clear sentence set from memory
 	del(sentence_sets[year])
@@ -78,12 +81,12 @@ del(sentence_sets)
 # print("Loading models...", end = "\r")
 # for year in range(start_year, end_year + 1) :
 	# try :
-		# model = gensim.models.Word2Vec.load("%s%d+%sx%d.word2vec" % (output_dir, year, sys.argv[3], dimensionality))
+		# model = gensim.models.Word2Vec.load("%s%d+%dx%d.word2vec" % (output_dir, year, window_len, dimensionality))
 		# models[year] = model.wv
 		# del(model)
 		# print("Loading models (%d - %d)" % (start_year, year), end = "\r")
 	# except :
-		# print("Fatal: No model found for %d (%s%d+%sx%d.word2vec)" % (year, output_dir, year, sys.argv[3], dimensionality), file = sys.stderr)
+		# print("Fatal: No model found for %d (%s%d+%dx%d.word2vec)" % (year, output_dir, year, window_len, dimensionality), file = sys.stderr)
 		# sys.exit(4)
 # print()
 
@@ -109,7 +112,7 @@ for word in base :
 print()
 
 # save overlap set
-output = open(output_dir + "overlap-%s-%s+%sx%d" % (sys.argv[1], sys.argv[2], sys.argv[3], dimensionality), "wb")
+output = open(output_dir + "overlap-%d-%d+%dx%d" % (start_year, end_year, window_len, dimensionality), "wb")
 pickle.dump(wordset, output)
 output.close()
 
@@ -136,7 +139,7 @@ for word in wordset :
 	# write exhaustive data to csv
 	if csv :
 		try :
-			with open("%s%s-%s-%s+%sx%dt%d.csv" % (output_dir, word, sys.argv[1], sys.argv[2], sys.argv[3], dimensionality, top_n), "w") as output :
+			with open("%s%s-%s-%s+%sx%dt%d.csv" % (output_dir, word, start_year, end_year, window_len, dimensionality, top_n), "w") as output :
 				print(",%s" % (",".join(map(str, range(start_year, end_year + 1)))), file = output)
 				for word in union :
 					print(word, file = output, end = ",")
@@ -144,7 +147,7 @@ for word in wordset :
 				print("", file = output)
 				output.close()
 		except :
-			print("Error: could not write file %s%s-%s-%s+%sx%dt%d.csv; skipping" % (output_dir, word, sys.argv[1], sys.argv[2], sys.argv[3], dimensionality, top_n), file = sys.stderr)
+			print("Error: could not write file %s%s-%s-%s+%sx%dt%d.csv; skipping" % (output_dir, word, start_year, end_year, window_len, dimensionality, top_n), file = sys.stderr)
 	
 	i += 1
 	if (100 * i // len(wordset)) > p :
@@ -159,7 +162,7 @@ drifters = sorted(dict_metric, key = dict_metric.get)
 print("Sorted    ")
 
 # save sorted list
-output = open(output_dir + "sorted-%s-%s+%sx%dt%d" % (sys.argv[1], sys.argv[2], sys.argv[3], dimensionality, top_n), "wb")
+output = open(output_dir + "sorted-%s-%s+%sx%dt%d" % (start_year, end_year, window_len, dimensionality, top_n), "wb")
 pickle.dump(drifters, output)
 output.close()
 
